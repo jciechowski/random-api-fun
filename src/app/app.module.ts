897abb8dffb8e313b0db58db2c20ab5185bbd67d
Jakub Ciechowski <sianex@gmail.com>
2017-09-02 09:32:50 +0200

    using urban dictionary api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -14,7 +15,8 @@ import { NavbarComponent } from './navbar/navbar.component';
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/map';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 const states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado',
   'Connecticut', 'Delaware', 'District Of Columbia', 'Federated States Of Micronesia', 'Florida', 'Georgia',
@@ -20,16 +21,38 @@ const states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'C
 })
 export class NavbarComponent implements OnInit {
   public model: any;
+
   search = (text$: Observable<string>) =>
     text$
       .debounceTime(200)
       .distinctUntilChanged()
       .map(term => term.length < 2 ? []
-        : states.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10));
+        : states.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
 
-  constructor() { }
+  constructor(private http: HttpClient) {
 
-  ngOnInit() {
   }
 
+  ngOnInit() {
+    const headerDict = {
+        'X-Mashape-Authorization': 'QwXbIDZpMjmshn9njwGtM2LJrhPJp1vzY84jsnbSVrXtBkW3MR',
+        'Accept': 'text/plain',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      };
+    const headerObj = {
+      headers: new HttpHeaders(headerDict),
+    };
+    this.http
+      .get('https://mashape-community-urban-dictionary.p.mashape.com/define?term=wat', headerObj)
+      .subscribe(data => {
+        console.log(data);
+      });
+  }
+
+  /*
+  curl --get --include 'https://mashape-community-urban-dictionary.p.mashape.com/define?term=wat' \
+    -H 'X-Mashape-Key: QwXbIDZpMjmshn9njwGtM2LJrhPJp1vzY84jsnbSVrXtBkW3MR' \
+    -H 'Accept: text/plain'
+
+   */
 }
