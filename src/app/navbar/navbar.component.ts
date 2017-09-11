49ea09a57cf569613c774c5b8a22e8ee265949eb
Jakub Ciechowski <sianex@gmail.com>
2017-09-11 07:06:07 +0200

    fixed search

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -14,6 +14,7 @@ import { UrbanDictionaryService } from '../urban-dictionary.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  public model: any;
   constructor(private urbanDictionaryService: UrbanDictionaryService) {
 
   }
